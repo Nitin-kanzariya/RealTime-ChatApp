c6356bd232da9920ec2f6407ac3aa1abd85c9968
Nitin-Kanzariya <nitinkanzariya112233@gmail.com>
2025-01-07 17:22:51 +0530

    Chat-app project completed

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,13 @@ import messageRoutes from "./routes/message.route.js";
 
 import { app, server } from "./lib/socket.js";
 
+
+import path from 'path';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -35,6 +39,15 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend", "dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port", PORT);
